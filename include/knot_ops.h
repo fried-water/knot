aa5620dd75e0249b615f9495ce53a80629d88a95
Leslie Barron <lesliebarron1@gmail.com>
2022-04-22 22:34:52 -0400

    Expand template type in knot op macro to avoid shadowing error

--- include/knot_ops.h
+++ include/knot_ops.h
@@ -1,54 +1,44 @@
 #ifndef KNOT_OPS
 #define KNOT_OPS
 
-#define KNOT_COMPAREABLE(U)                                                     \
-  template <typename T>                                                         \
-  std::enable_if_t<std::is_same_v<U, T>, bool> operator==(const T& rhs) const { \
-    using knot::as_tie;                                                         \
-    return as_tie(*this) == as_tie(rhs);                                        \
-  }                                                                             \
-                                                                                \
-  template <typename T>                                                         \
-  std::enable_if_t<std::is_same_v<U, T>, bool> operator!=(const T& rhs) const { \
-    using knot::as_tie;                                                         \
-    return as_tie(*this) != as_tie(rhs);                                        \
+#define KNOT_COMPAREABLE(U)                                                               \
+  template <typename KNOT_T>                                                              \
+  std::enable_if_t<std::is_same_v<U, KNOT_T>, bool> operator==(const KNOT_T& rhs) const { \
+    using knot::as_tie;                                                                   \
+    return as_tie(*this) == as_tie(rhs);                                                  \
+  }                                                                                       \
+                                                                                          \
+  template <typename KNOT_T>                                                              \
+  std::enable_if_t<std::is_same_v<U, KNOT_T>, bool> operator!=(const KNOT_T& rhs) const { \
+    using knot::as_tie;                                                                   \
+    return as_tie(*this) != as_tie(rhs);                                                  \
   }
 
-#define KNOT_ORDERED(U)                                                         \
-  template <typename T>                                                         \
-  std::enable_if_t<std::is_same_v<U, T>, bool> operator==(const T& rhs) const { \
-    using knot::as_tie;                                                         \
-    return as_tie(*this) == as_tie(rhs);                                        \
-  }                                                                             \
-                                                                                \
-  template <typename T>                                                         \
-  std::enable_if_t<std::is_same_v<U, T>, bool> operator!=(const T& rhs) const { \
-    using knot::as_tie;                                                         \
-    return as_tie(*this) != as_tie(rhs);                                        \
-  }                                                                             \
-                                                                                \
-  template <typename T>                                                         \
-  std::enable_if_t<std::is_same_v<U, T>, bool> operator<(const T& rhs) const {  \
-    using knot::as_tie;                                                         \
-    return as_tie(*this) < as_tie(rhs);                                         \
-  }                                                                             \
-                                                                                \
-  template <typename T>                                                         \
-  std::enable_if_t<std::is_same_v<U, T>, bool> operator<=(const T& rhs) const { \
-    using knot::as_tie;                                                         \
-    return as_tie(*this) <= as_tie(rhs);                                        \
-  }                                                                             \
-                                                                                \
-  template <typename T>                                                         \
-  std::enable_if_t<std::is_same_v<U, T>, bool> operator>(const T& rhs) const {  \
-    using knot::as_tie;                                                         \
-    return as_tie(*this) > as_tie(rhs);                                         \
-  }                                                                             \
-                                                                                \
-  template <typename T>                                                         \
-  std::enable_if_t<std::is_same_v<U, T>, bool> operator>=(const T& rhs) const { \
-    using knot::as_tie;                                                         \
-    return as_tie(*this) >= as_tie(rhs);                                        \
+#define KNOT_ORDERED(U)                                                                   \
+  KNOT_COMPAREABLE(U)                                                                     \
+                                                                                          \
+  template <typename KNOT_T>                                                              \
+  std::enable_if_t<std::is_same_v<U, KNOT_T>, bool> operator<(const KNOT_T& rhs) const {  \
+    using knot::as_tie;                                                                   \
+    return as_tie(*this) < as_tie(rhs);                                                   \
+  }                                                                                       \
+                                                                                          \
+  template <typename KNOT_T>                                                              \
+  std::enable_if_t<std::is_same_v<U, KNOT_T>, bool> operator<=(const KNOT_T& rhs) const { \
+    using knot::as_tie;                                                                   \
+    return as_tie(*this) <= as_tie(rhs);                                                  \
+  }                                                                                       \
+                                                                                          \
+  template <typename KNOT_T>                                                              \
+  std::enable_if_t<std::is_same_v<U, KNOT_T>, bool> operator>(const KNOT_T& rhs) const {  \
+    using knot::as_tie;                                                                   \
+    return as_tie(*this) > as_tie(rhs);                                                   \
+  }                                                                                       \
+                                                                                          \
+  template <typename KNOT_T>                                                              \
+  std::enable_if_t<std::is_same_v<U, KNOT_T>, bool> operator>=(const KNOT_T& rhs) const { \
+    using knot::as_tie;                                                                   \
+    return as_tie(*this) >= as_tie(rhs);                                                  \
   }
 
 #endif
